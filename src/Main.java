35f026e3b5311bb8f0ba39b1078348b53814b7ee
Gab Delgado <gabreldelgado2002@gmail.com>
2021-03-12 19:38:38 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public Main() {
+		
+	}
+
+	public static void main(String[] args) {
+		Main ppal = new Main();
+		ppal.executeProgram();
+
+	}
+	
+	public void executeProgram() {
+		//int n, m;
+	}
+	
+	public String result (int [] arr, int m) {
+		return "";
+		
+	}
+
+}
